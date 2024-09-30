eaa88936c3d974f702cddfac4035c6438171a535
KK2292 <kohei.kwn@gmail.com>
2024-09-30 14:42:36 +0900

    Refactor animation and particle configuration, and update index.js and style.css

--- js/index.js
+++ js/index.js
@@ -1,11 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const subTitle = document.getElementsByClassName("sub-title")[0];
-  const text = subTitle.textContent;
-  subTitle.innerHTML = "";
-  text.split("").forEach((char, index) => {
-    const span = document.createElement("span");
-    span.textContent = char;
-    span.style.animationDelay = `${index * 0.1}s`;
-    subTitle.appendChild(span);
-  });
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.querySelector(".first-view-contents").style.backgroundColor =
+      "rgba(0, 0, 0, 0.5)";
+  }, 2500);
+});
+
+const typeTarget = document.querySelectorAll(".sub-title");
+
+typeTarget.forEach((element) => {
+  let typeContent = element.textContent;
+  let typeSprit = typeContent.split("");
+  element.textContent = "";
+  let typeLength = 0;
+
+  const typeFunction = () => {
+    const typeInterval = setInterval(() => {
+      if (typeLength < typeSprit.length) {
+        if (typeSprit[typeLength - 1] === ",") {
+          clearInterval(typeInterval);
+          setTimeout(() => {
+            element.textContent += typeSprit[typeLength];
+            typeLength++;
+            typeFunction();
+          }, 500);
+        } else {
+          element.textContent += typeSprit[typeLength];
+          typeLength++;
+        }
+      } else {
+        clearInterval(typeInterval);
+      }
+    }, 70);
+  };
+
+  setTimeout(typeFunction, 3200);
 });
